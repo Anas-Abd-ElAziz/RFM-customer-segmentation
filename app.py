02ea6a89d69be8a70d1e45da7f572103b65b0930
Anas-Abd-ElAziz <mohamedanas709@gmail.com>
2023-08-04 14:40:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ import joblib
 kmeans = joblib.load('rfm_model.pkl')
 df = pd.read_csv('clusters.csv')
 
-def recommend_merchant(user_id):
-	user_data = df[df['User_Id'] == user_id]
-	cluster_label = user_data['Cluster'].iloc[0]
-	recommended_merchant = user_data[user_data['Cluster'] == cluster_label]['Mer_Id'].mode().iloc[0]
-	return recommended_merchant
+def recommend_merchants_with_category(user_id):
+    user_data = df[df['User_Id'] == user_id]
+    cluster_label = user_data['Cluster'].iloc[0]
+    recommended_category = user_data[user_data['Cluster'] == cluster_label]['Category In English'].mode().iloc[0]
+    top_merchants = df[(df['Cluster'] == cluster_label) & (df['Category In English'] == recommended_category)]['Mer_Id'].value_counts().nlargest(5).index.tolist()
+    return top_merchants
 
 # Function to recommend a category based on user ID
 def recommend_category(user_id):
@@ -35,13 +36,14 @@ def main():
 
     if user_id is not None:
         st.write(f"Recommendations for User ID: {user_id}")
-        st.subheader("Recommended Merchant:")
-        top_merchants = recommend_merchant(user_id)
-        st.write(top_merchants)
-
+		
         st.subheader("Recommended Category:")
         top_categories = recommend_category(user_id)
         st.write(top_categories)
+		
+        st.subheader("Recommended Merchants based on that category:")
+        top_merchants = recommend_merchants_with_category(user_id)
+        st.write(top_merchants)
     else:
         st.write("Please enter a valid User ID.")
 
